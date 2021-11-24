2a83ade1ed068858c7830bafa4a5a3e4c6e0cdf3
anthony <anthony@MacBook-Pro-de-coding.local>
2021-11-24 15:40:27 +0100

    Cleanup some code

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
-//const indexRouter = require('./routes/index');
 const userRouter = require('./routes/userRouter');
 const config = require('./config');
 const nunjucks = require('nunjucks')
-const mongoose = require('mongoose')
-//const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
 const app = express()
 const port = config.webPort
 const host = config.webHost
@@ -15,32 +13,18 @@ nunjucks.configure("views", {
   express: app
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({express: true}));
 
 app.listen(port, host, () => {
   console.log(`Example app listening at ${protocol}://${host}:${port}`)
 })
 
-// Home page route.
-app.get('/test', function (req, res) {
-    res.send('Wiki home page');
-})
-
-// About page route.
-app.get('/about', function (req, res) {
-  res.send('About this wiki');
-})
-
 //app.use('/', indexRouter);
 app.use('/user', userRouter);
-
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded());
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
 
 //app.get('/user', userRouter);
 //app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
--- controllers/userController.js
+++ controllers/userController.js
@@ -14,20 +14,21 @@ exports.user_detail = function(req, res) {
 // Display User create form on GET.
 exports.user_create_get = function(req, res) {
     res.render("create_user.html")
+
+    let test = "ABCdef#1234"
+    if (test.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$")) {
+        console.log("YEEES !")
+    } else {
+        console.log("NOOOOOO")
+    }
     //res.sendFile('/views/create_user.html', {root: path.dirname(__dirname)});
 };
 
 // Handle User create on POST.
 exports.user_create_post = function(req, res) {
-    //console.log(req)
-    //requestdb()
-    //console.log(req.body.id)
     res.send('NOT IMPLEMENTED: User create POST');
-};
 
-async function requestdb() {
-  console.log("TEST")
-}
+};
 
 // Display User delete form on GET.
 exports.user_delete_get = function(req, res) {
