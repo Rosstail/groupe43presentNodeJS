cab24023afe2ac3735ad1b2a9f6521b902db804e
Jimmy <ljimmy98@hotmail.fr>
2021-11-26 09:49:24 +0100

    multiple chat room

--- views/client.js
+++ views/client.js
@@ -22,3 +22,49 @@ $('#formgeneral').submit(function (e) {
 socket.on('chatgeneral-mesgeneral', function (message) {
   $('#mesgeneral').append($('<li>').text(message.text));
 });
+
+
+/**
+ * Envoi d'un message
+ */
+$('#formgame').submit(function (e) {
+  e.preventDefault();
+  var message = {
+    text : $('#sendgame').val()
+  };
+  $('#sendgame').val('');
+  if (message.text.trim().length !== 0) { // Gestion message vide
+    socket.emit('chatgeneral-mesgame', message);
+  }
+  $('#chatgame input').focus(); // Focus sur le champ du message
+});
+
+/**
+ * Réception d'un message
+ */
+socket.on('chatgeneral-mesgame', function (message) {
+  $('#mesgame').append($('<li>').text(message.text));
+});
+
+
+/**
+ * Envoi d'un message
+ */
+$('#formwork').submit(function (e) {
+  e.preventDefault();
+  var message = {
+    text : $('#sendwork').val()
+  };
+  $('#sendwork').val('');
+  if (message.text.trim().length !== 0) { // Gestion message vide
+    socket.emit('chatgeneral-meswork', message);
+  }
+  $('#chatwork input').focus(); // Focus sur le champ du message
+});
+
+/**
+ * Réception d'un message
+ */
+socket.on('chatgeneral-meswork', function (message) {
+  $('#meswork').append($('<li>').text(message.text));
+});
